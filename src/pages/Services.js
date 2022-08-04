a0edd02bcf4180930c43ffde1400317534440730
igoryen <igor.yentaltsev@gmail.com>
2022-08-04 00:08:12 -0400

    Added tabs for tours on p Services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -4,6 +4,10 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import CarRide from "../components/CarRide";
 import Testimonials from "../components/Testimonials";
 
+// https://www.npmjs.com/package/react-tabs
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 // carousel for Habana
 import imgCapitolio from "../images/slideshow/habana/capitolio.jpg";
 import imgCastillio from "../images/slideshow/habana/castillo.jpg";
@@ -114,7 +118,12 @@ class Services extends React.Component {
 
         return (
 
+
+
+
             <div className="page services">
+
+
 
                 <section className="page__heading">
                     <div className="element heading">
@@ -124,168 +133,199 @@ class Services extends React.Component {
 
                 <CarRide />
 
-                <section className="page__section page__section_a">
-                    <div className="element heading">
-                        <h2>Гавана (160 &euro;)</h2>
-                    </div>
+                <section className="page__section_tabs">
+                    <p>Гавана (Г), Карибы (К), Матансас (М), Тринидад и Сьенфуэгос (Т и С)</p>
+                </section>
+                <Tabs className="my-tabs" defaultFocus="true">
+                    <TabList className="my-tab-list">
+                        <Tab><span className="mobile">Г</span><span className="desktop">Гавана</span></Tab>
+                        <Tab><span className="mobile">К</span><span className="desktop">Карибы</span></Tab>
+                        <Tab><span className="mobile">М</span><span className="desktop">Матансас</span></Tab>
+                        <Tab><span className="mobile">Т и С</span><span className="desktop">Тринидад и Сьенфуэгос</span></Tab>
+                    </TabList>
 
-                    <div className="my-carousel">
-                        <AliceCarousel mouseTracking
-                            items={itemsHabana}
-                            autoPlay='true'
-                            infinite='true'
-                            autoPlayInterval='2000'
-                            autoHeight='true'
-                            responsive={responsivity}
-                            disableButtonsControls='false'
-                        // paddingLeft="10"
-                        // paddingRight='10'
-                        />
-                    </div>
 
-                    <div className="element description">
 
-                        <div className="part">
-                            <div className="part-bit">
-                                <h3>Транспорт</h3>
+
+                    <TabPanel>
+
+                        <section className="page__section page__section_a">
+                            <div className="element heading">
+                                <h2>Гавана (160 &euro;)</h2>
                             </div>
-                            <div className="part-bit">
-                                <div>
-                                    <h3>Ретро</h3>
-                                    <p>1-4 человека</p>
-                                    <p>Цена 160 &euro; (за всех кто в машине )</p>
-                                    <p>В стоимость входит: транспорт с водителем, бензин и мои услуги гида.</p>
-                                    <p>В салон Ретро автомобиля помещается 4 туриста, не считая водителя и меня.
-                                        И не важно сколько человек в машине, один или 4, цена 160 &euro;.</p>
-                                    <p>В автомобиле есть кондиционер.</p>
+
+                            <div className="my-carousel">
+                                <AliceCarousel mouseTracking
+                                    items={itemsHabana}
+                                    autoPlay='true'
+                                    infinite='true'
+                                    autoPlayInterval='2000'
+                                    autoHeight='true'
+                                    responsive={responsivity}
+                                    disableButtonsControls='false'
+                                // paddingLeft="10"
+                                // paddingRight='10'
+                                />
+                            </div>
+
+                            <div className="element description">
+
+                                <div className="part">
+                                    <div className="part-bit">
+                                        <h3>Транспорт</h3>
+                                    </div>
+                                    <div className="part-bit">
+                                        <div>
+                                            <h3>Ретро</h3>
+                                            <p>1-4 человека</p>
+                                            <p>Цена 160 &euro; (за всех кто в машине )</p>
+                                            <p>В стоимость входит: транспорт с водителем, бензин и мои услуги гида.</p>
+                                            <p>В салон Ретро автомобиля помещается 4 туриста, не считая водителя и меня.
+                                                И не важно сколько человек в машине, один или 4, цена 160 &euro;.</p>
+                                            <p>В автомобиле есть кондиционер.</p>
+                                        </div>
+                                        <div>
+                                            <h3>Современная (такси)</h3>
+                                            <p>1-3 человека</p>
+                                            <p>Более комфортно</p>
+                                            <p>Цена 160 &euro; (за всех кто в машине )</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div>
-                                    <h3>Современная (такси)</h3>
-                                    <p>1-3 человека</p>
-                                    <p>Более комфортно</p>
-                                    <p>Цена 160 &euro; (за всех кто в машине )</p>
+                                <div className="part">
+                                    <div className="part-bit">
+                                        <h3>Содержание экскурсии</h3>
+                                    </div>
+                                    <div className="part-bit">
+                                        <div>
+                                            <h3>Отправление</h3>
+                                            Выезд из отеля в 8 утра</div>
+                                        <div>
+                                            <h3>Посещение основных достопримечательностей столицы Кубы</h3>
+                                            <ol>
+                                                <li>осмотр Гаваны на возвышенности где находится монумент Иисуса Христа благославляющего Гавану</li>
+                                                <li>пешая прогулка: колониальных часть Гаваны: здания, площади, крепости возведенные испанцами в XVI - XIX веках</li>
+                                                <li>Обед (в стоимость экскурсии не входит)</li>
+                                                <li>Осмотр достопримечательностей современной Гаваны: Капитолия, Президентского дворца, Площади Революции, центральных улиц и знаменитой набережной города - Малекон.</li>
+                                            </ol>
+
+                                        </div>
+                                        <div>
+                                            <h3>Возвращение</h3>
+                                            <p>6 часов вечера</p>
+                                        </div>
+                                        <div>
+                                            <h3>Примечание</h3>
+                                            <p>Принимаются предложения. За дополнительное время и расстояния - доплата</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="part">
-                            <div className="part-bit">
-                                <h3>Содержание экскурсии</h3>
-                            </div>
-                            <div className="part-bit">
-                                <div>
-                                    <h3>Отправление</h3>
-                                    Выезд из отеля в 8 утра</div>
-                                <div>
-                                    <h3>Посещение основных достопримечательностей столицы Кубы</h3>
-                                    <ol>
-                                        <li>осмотр Гаваны на возвышенности где находится монумент Иисуса Христа благославляющего Гавану</li>
-                                        <li>пешая прогулка: колониальных часть Гаваны: здания, площади, крепости возведенные испанцами в XVI - XIX веках</li>
-                                        <li>Обед (в стоимость экскурсии не входит)</li>
-                                        <li>Осмотр достопримечательностей современной Гаваны: Капитолия, Президентского дворца, Площади Революции, центральных улиц и знаменитой набережной города - Малекон.</li>
-                                    </ol>
+
+                                <div className="part">
+                                    <div className="part-bit">
+                                        <h3>Примечание</h3>
+                                    </div>
+                                    <div className="part-bit">
+                                        <p>
+                                            Рекомендую бронировать день экскурсии заранее
+                                        </p>
+                                        <p>
+                                            Попутчиков не ищу
+                                        </p>
+                                        <p>
+                                            Проверяю почту обычно вечером по кубинскому времени
+                                        </p>
+                                    </div>
 
                                 </div>
-                                <div>
-                                    <h3>Возвращение</h3>
-                                    <p>6 часов вечера</p>
-                                </div>
-                                <div>
-                                    <h3>Примечание</h3>
-                                    <p>Принимаются предложения. За дополнительное время и расстояния - доплата</p>
-                                </div>
                             </div>
-                        </div>
+                        </section>
 
-                        <div className="part">
-                            <div className="part-bit">
-                                <h3>Примечание</h3>
+                    </TabPanel>
+                    <TabPanel>
+
+                        <section className="page__section page__section_b">
+                            <div className="element heading">
+                                <h2>Карибы (160 &euro;)</h2>
                             </div>
-                            <div className="part-bit">
+                            <div className="element description">
                                 <p>
-                                    Рекомендую бронировать день экскурсии заранее
+                                    (Крокодиловая ферма)  евро на ретроавтомобиле. 160 на современном автомобиле.
                                 </p>
+                            </div>
+                        </section>
+
+                    </TabPanel>
+                    <TabPanel>
+
+                        <section className="page__section page__section_c">
+                            <div className="element heading">
+                                <h2>Город Матансас (130 &euro;)</h2>
+                            </div>
+
+                            <div className="my-carousel">
+                                <AliceCarousel mouseTracking
+                                    items={itemsMatanzas}
+                                    autoPlay='true'
+                                    infinite='true'
+                                    autoPlayInterval='2000'
+                                    autoHeight='true'
+                                    responsive={responsivity}
+                                    disableButtonsControls='false'
+                                // paddingLeft="10"
+                                // paddingRight='10'
+                                />
+                            </div>
+
+                            <div className="element description">
                                 <p>
-                                    Попутчиков не ищу
+                                    (город, река, пещера)
                                 </p>
+                            </div>
+                        </section>
+                    </TabPanel>
+                    <TabPanel>
+
+                        <section className="page__section page__section_d">
+                            <div className="element heading">
+                                <h2>Города Тринидад и Сьенфуэгос (450 &euro;)</h2>
+                            </div>
+
+                            <div className="my-carousel">
+                                <AliceCarousel mouseTracking
+                                    items={itemsTriCien}
+                                    autoPlay='true'
+                                    infinite='true'
+                                    autoPlayInterval='2000'
+                                    autoHeight='true'
+                                    responsive={responsivity}
+                                    disableButtonsControls='false'
+                                // paddingLeft="10"
+                                // paddingRight='10'
+                                />
+                            </div>
+
+                            <div className="element description">
                                 <p>
-                                    Проверяю почту обычно вечером по кубинскому времени
+                                    за экскурсию на минивэн (9 мест)
                                 </p>
                             </div>
 
-                        </div>
-                    </div>
-                </section>
+                            <div className="element description">
+                                <p>
+                                    Обед и входные билеты за отдельную плату
 
-                <section className="page__section page__section_b">
-                    <div className="element heading">
-                        <h2>Карибы (160 &euro;)</h2>
-                    </div>
-                    <div className="element description">
-                        <p>
-                            (Крокодиловая ферма)  евро на ретроавтомобиле. 160 на современном автомобиле.
-                        </p>
-                    </div>
-                </section>
-
-                <section className="page__section page__section_c">
-                    <div className="element heading">
-                        <h2>Город Матансас (130 &euro;)</h2>
-                    </div>
-
-                    <div className="my-carousel">
-                        <AliceCarousel mouseTracking
-                            items={itemsMatanzas}
-                            autoPlay='true'
-                            infinite='true'
-                            autoPlayInterval='2000'
-                            autoHeight='true'
-                            responsive={responsivity}
-                            disableButtonsControls='false'
-                        // paddingLeft="10"
-                        // paddingRight='10'
-                        />
-                    </div>
-
-                    <div className="element description">
-                        <p>
-                            (город, река, пещера)
-                        </p>
-                    </div>
-                </section>
+                                </p>
+                            </div>
+                        </section>
+                    </TabPanel>
 
 
-                <section className="page__section page__section_d">
-                    <div className="element heading">
-                        <h2>Города Тринидад и Сьенфуэгос (450 &euro;)</h2>
-                    </div>
 
-                    <div className="my-carousel">
-                        <AliceCarousel mouseTracking
-                            items={itemsTriCien}
-                            autoPlay='true'
-                            infinite='true'
-                            autoPlayInterval='2000'
-                            autoHeight='true'
-                            responsive={responsivity}
-                            disableButtonsControls='false'
-                        // paddingLeft="10"
-                        // paddingRight='10'
-                        />
-                    </div>
 
-                    <div className="element description">
-                        <p>
-                            за экскурсию на минивэн (9 мест)
-                        </p>
-                    </div>
 
-                    <div className="element description">
-                        <p>
-                            Обед и входные билеты за отдельную плату
 
-                        </p>
-                    </div>
-                </section>
+                </Tabs>
 
                 <Testimonials />
 
